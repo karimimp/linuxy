546067044c18fa8ffd781b66d68f9ccc3a8fa0d4
mohsen <mohsenk@outlook.com>
2020-12-22 11:16:12 +0100

    common conda commands

---
+++ conda_stuff.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Keeping a list of useful commands for conda
+
+# list all available packages
+conda list
+
+# conda version
+conda --version
+
+# search package name
+conda search "packagename"
+
+# install packages
+conda install -c "packagename"
