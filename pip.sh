31ff1a7543909ec41d438e9da06c4f3a1e81369b
Mohsen <mohsenk@outlook.com>
2024-08-20 11:15:18 +0200

    Create pip.sh

---
+++ pip.sh
@@ -0,0 +1,25 @@
+# pip cheat sheet
+
+# install a package
+pip install PACKAGE_NAME
+
+# uninstall a package
+pip uninstall PACKAGE_NAME
+
+# update a package
+pip install --upgrade PACKAGE_NAME
+
+# list all packages
+pip list
+
+# list outdated packages
+pip list --outdated
+
+# show package info
+pip show PACKAGE_NAME
+
+# upgrade pip
+pip install --upgrade pip
+
+# show installed packages in requirements format
+pip freeze
